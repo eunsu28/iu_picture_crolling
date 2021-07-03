c37b113529e95f36526a0c6a4ffd0fddc84a7083
eunsu28 <eunsuvg@gmail.com>
2021-07-03 16:54:40 +0800

    어렵네

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+limited = 20
 url = "https://www.google.co.kr/search?q=%EC%95%84%EC%9D%B4%EC%9C%A0&newwindow=1&safe=strict&sxsrf=ALeKk00eq3WpKxyIYWLL554xWgnSYmnSLg:1625297891531&source=lnms&tbm=isch&sa=X&ved=2ahUKEwim4em7ssbxAhXW73MBHexFCS4Q_AUoAXoECAEQAw&biw=1870&bih=965"
 
 result = requests.get(url)
 soup = BeautifulSoup(result.text, "html.parser")
 
-picture = soup.find_all("div", {"class": "bRMDJf islir"}, limit = 20)
+# imgs = soup.select('img') #select 는 css를 찾는 방식
 
-print(picture)
+div = soup.find("div", {"class": "bRMDJf islir"})
+
+img = soup.find_all("img")
+
+for i in img:
+    print(img)
