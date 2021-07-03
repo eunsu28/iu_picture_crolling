6ebb88e5a7a3e96aee0bf8c2beca3faadec21f5c
eunsu28 <eunsuvg@gmail.com>
2021-07-03 19:36:43 +0800

    back to google

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 
-url = "https://search.naver.com/search.naver?where=image&sm=tab_jum&query=%EC%95%84%EC%9D%B4%EC%9C%A0"
+url = "https://www.google.com.hk/search?q=%EC%95%84%EC%9D%B4%EC%9C%A0&newwindow=1&safe=strict&sxsrf=ALeKk02Bzz8MyG6co35WF6lyBlv7qU76Zw:1625311624983&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjrmbnQ5cbxAhVOILcAHdPxD-8Q_AUoAXoECAEQAw&biw=1920&bih=969"
 
 result = requests.get(url)
 soup = BeautifulSoup(result.text, "html.parser")
-a = soup.find_all("li", {"class" : "menu"})
-for i in a:
-    print(i.text)
+img = soup.find_all("img")
+for i in img:
+    print(img)
 
 
