b3213595f6bcb639062d3761b2f069c79e0c5676
eunsu28 <eunsuvg@gmail.com>
2021-07-03 20:21:16 +0800

    almost

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
-import urllib.request
+import urllib.request 
+
 
 url = "https://www.google.com.hk/search?q=%EC%95%84%EC%9D%B4%EC%9C%A0&newwindow=1&safe=strict&sxsrf=ALeKk02Bzz8MyG6co35WF6lyBlv7qU76Zw:1625311624983&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjrmbnQ5cbxAhVOILcAHdPxD-8Q_AUoAXoECAEQAw&biw=1920&bih=969"
 
 result = requests.get(url)
 soup = BeautifulSoup(result.text, "html.parser")
-img = soup.find_all("img", {"class" : "rg_i Q4LuWd"})
-for i in img:
-    print(img)
-    if i == None:
-        print("none")
+img = soup.find_all("img")
+n = 1
+
+a = img[1:]
+# img = soup.find("img")  # 이미지 태그
+# img_src = a.get("src")
+# print(img_src)
 
 
+for i in a:
+    img_src = i.get("src")
+    urllib.request.urlretrieve(img_src, f"{img_src}.png")
+    # urllib.request.urlretrieve(url + img_src, "/Users/kim-eunsu/Desktop/구글 이미지 크롤링/img")
+    
