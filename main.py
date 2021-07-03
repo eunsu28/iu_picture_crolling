c66a7a2c9f99a5debf71ec6e4ac5d834d627fad6
eunsu28 <eunsuvg@gmail.com>
2021-07-03 16:06:05 +0800

    아이유 사진 크롤링

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.google.co.kr/search?q=%EC%95%84%EC%9D%B4%EC%9C%A0&newwindow=1&safe=strict&sxsrf=ALeKk00eq3WpKxyIYWLL554xWgnSYmnSLg:1625297891531&source=lnms&tbm=isch&sa=X&ved=2ahUKEwim4em7ssbxAhXW73MBHexFCS4Q_AUoAXoECAEQAw&biw=1870&bih=965"
+
+result = requests.get(url)
+soup = BeautifulSoup(result.text, "html.parser")
+
+print(soup)
